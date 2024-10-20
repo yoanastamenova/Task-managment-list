1e7bd5b70a1f8a6f408fbaf3d667ee3b51137cda
Yoana <yoanabarbie@icloud.com>
2024-10-20 18:11:17 +0200

    feat: used interface for the user object

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,15 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -6,19 +17,15 @@ import { Component, EventEmitter, Input, Output, output } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-@Input({ required: true }) user!: {
-  id: string;
-  name: string;
-  avatar: string;
-}
+@Input({ required: true }) user!: User;
  @Output() select = new EventEmitter();     //EventEmiter object allows us to emit custom values throught that select property we issued
 // select = output<string>();              //using generic type again
 
  get imagePath() {
-  return 'assets/users/' + this.avatar;
+  return 'assets/users/' + this.user.avatar;
  }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
