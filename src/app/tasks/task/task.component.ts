00b85c12ec1f0f17170f9e677bcb65e916b3fad7
Yoana <yoanabarbie@icloud.com>
2024-10-21 14:09:41 +0200

    feat: displaying task info now

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input } from '@angular/core';
 
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -8,5 +15,5 @@ import { Component, Input } from '@angular/core';
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
-  
+  @Input({required: true}) task!: Task;
 }
