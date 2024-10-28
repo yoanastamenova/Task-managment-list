cb3abfd0e1bb50c430d79b260204758255f84469
Yoana <yoanabarbie@icloud.com>
2024-10-28 11:57:19 +0100

    refactor: changed the components to Modules in order to see and learn how Modules work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { CardComponent } from './shared/card/card.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ],
   bootstrap: [AppComponent],
-  imports: [HeaderComponent, UserComponent, TasksComponent]
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
